2eb4872b794231eb787e68d2adad0ccdc2806fb4
Rafael-Friedel <RafaFriedelDev@outlook.com>
2022-03-22 18:16:55 -0300

    REQUISITO 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from './RenderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('TESTANDO COMPONENTE NOTFOUND', () => {
+  it('Testa se a página contém um h2 com o texto Page requested not found 😭', () => {
+    RenderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', {
+      level: 2, name: /Page requested not found/i,
+    });
+    const span = screen.getByText('😭');
+    expect(heading).toBeInTheDocument();
+    expect(span).toBeInTheDocument();
+  });
+  it('Testa se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    RenderWithRouter(<NotFound />);
+    const imag = screen.getAllByRole('img');
+    expect(imag[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
